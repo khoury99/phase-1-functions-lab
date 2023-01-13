79b048f307e1aec99e5e7f524ffc01da629964cf
Elijah Green <elijah.khoury99@gmail.com>
2023-01-13 09:40:57 -0800

    commit tree 652ed0b9939fdc31011112f8d9b7a4755d0a1054

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+  function distanceFromHqInBlocks(x) {
+  // x is my current block
+ if (x>42){
+return x-42
+ }
+ else if (x<42){
+return 42-x
+ }
+}
+
+console.log(distanceFromHqInBlocks(41))
